2c430e4253f86fb3317344def88b18e2a408836a
monore <goldpocket01@gmail.com>
2020-03-23 05:06:02 +0900

    p32 算出プロパティの使い方まで学習

--- js/myScripts.js
+++ js/myScripts.js
@@ -4,3 +4,77 @@ let app = new Vue({
         message: 'hello world!'
     }
 })
+
+let app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: 'hello world2!'
+    }
+})
+
+let app3 = new Vue({
+    el: '#app3',
+    data: {
+        message: 'hello world2!'
+    }
+})
+
+let app4 = new Vue({
+    el: '#app4',
+    data: {
+        url: 'https://wings.msn.to/'
+    }
+})
+
+let app5 = new Vue({
+    el: '#app5',
+    data: {
+        flag: true,
+        flag2: false
+    }
+})
+
+let app6 = new Vue({
+    el: '#app6',
+    data: {
+        email: 'Y-Suzuki@example.com'
+    },
+    computed: {
+        localEmail: function () {
+            return this.email.split('@')[0].toLowerCase();
+        }
+    }
+})
+
+let app7 = new Vue({
+    el: '#app7',
+    data: {
+        email: 'Y-SUzuki@example.com'
+    },
+    methods: {
+        localEmail: function () {
+            return this.email.split('@')[0].toLowerCase();
+        }
+    }
+})
+
+
+let app8 = new Vue({
+    el: '#app8',
+    data: {
+        current: new Date().toLocaleString()
+    },
+    computed: {
+        randomc: function () {
+            return Math.random();
+        }
+    },
+    methods: {
+        onclick: function () {
+            this.current = new Date().toLocaleString();
+        },
+        randomm: function () {
+            return Math.random();
+        }
+    }
+})
